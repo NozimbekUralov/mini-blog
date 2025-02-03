7853c3ead8bc126f3b304a99e07a12bbb4d9ddeb
NozimbekUralov <nozimurolov1@gmail.com>
2025-02-04 00:29:15 +0500

    done

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-import json
-from flask import Flask, render_template, request
+import json, datetime
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        title = request.form['search']
+    
+        with open("data.json") as f:
+            posts = json.loads(f.read())
+
+        posts = list(filter(lambda post: title.lower() in post['title'].lower(), posts))
+
+        return render_template("index.html", posts=posts)
+
     return render_template("index.html")
 
-@app.route("/posts", methods=["GET", "POST"])
+@app.route("/posts")
 def blogs():
     with open("data.json") as f:
         posts = json.loads(f.read())
-
-    if request.method == 'POST':
-        title = request.form['search']
-        posts = filter(lambda post: title.lower() in post['title'].lower(), posts)
 
     return render_template("posts.html", posts=posts)
 
@@ -28,6 +34,41 @@ def post_detail(post_id):
     
     return render_template('detail.html', post=post)
 
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        title = request.form['title']
+        content = request.form['content']
+        created_at = str(datetime.datetime.now().isoformat())
+
+        with open("data.json") as f:
+            posts = json.loads(f.read())
+
+        posts.append({
+            "id": len(posts) + 1,
+            "title": title,
+            "content": content,
+            "created_at": created_at
+        })
+
+        with open("data.json", "w") as f:
+            f.write(json.dumps(posts, indent=4))
+
+        return render_template("add.html")
+
+    return render_template("add.html")
+
+@app.route("/delete/<int:post_id>")
+def delete(post_id):
+    with open("data.json") as f:
+        posts = json.loads(f.read())
+
+    posts = list(filter(lambda post: post['id'] != post_id, posts))
+
+    with open("data.json", "w") as f:
+        f.write(json.dumps(posts, indent=4))
+
+    return redirect("/posts")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
